7c7a203ad899128b3b3e486187c723b33ca90c25
Fredrik Andersson <fredrik.andersson@widespace.com>
2016-12-26 21:31:03 +0100

    Modularized the database connection

---
+++ src/db.js
@@ -0,0 +1,16 @@
+const MongoClient = require('mongodb').MongoClient;
+
+function connect() {
+  const MONGO_URL = process.env.MONGO_URL || null;
+
+  return new Promise((resolve, reject) => {
+    MongoClient.connect(MONGO_URL, function (err, db) {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(db);
+    });
+  });
+}
+
+module.exports = connect;
