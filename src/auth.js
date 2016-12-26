6bfc8f2fec428d35d919c589760c2ff438c61bdd
Fredrik Andersson <fredrik.andersson@widespace.com>
2016-12-26 21:19:14 +0100

    Added auth module

---
+++ src/auth.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+
+const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN || null;
+const AUTH0_CLIENT = process.env.AUTH0_CLIENT || null;
+const AUTH0_SECRET = process.env.AUTH0_SECRET || null;
+
+const credentials = {
+  audience: AUTH0_CLIENT,
+  issuer: `https://${AUTH0_DOMAIN}/`
+};
+
+function authenticate(token) {
+  return new Promise((resolve, reject) => {
+    if (!token) {
+      reject('Authorization token missing.');
+    }
+
+    jwt.verify(
+      token.replace('Bearer ', ''),
+      new Buffer(AUTH0_SECRET, 'base64'),
+      credentials,
+      (err, res) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(res.sub);
+      }
+    );
+  });
+}
+
+module.exports = authenticate;
